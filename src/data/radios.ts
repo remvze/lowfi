951ea8a3ebac88a199ca0ce8db2afaa0f9225f3c
Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
2024-05-24 01:42:02 +0200

    feat(addition of all lofi girl streams): adding all existing lofi girl streams into the list
    Addition of all lofi girl streams for addition into the program for completion sake

--- src/data/radios.ts
+++ src/data/radios.ts
@@ -7,4 +7,16 @@ export const radios = [
     title: 'Lofi Girl — synthwave radio',
     url: 'https://www.youtube.com/watch?v=4xDzrJKXOOY',
   },
+  {
+    title: 'Lofi Girl — focus radio',
+    url: 'https://www.youtube.com/watch?v=4oStw0r33so',
+  },
+  {
+    title: 'Lofi Girl —  dream radio',
+    url: 'https://www.youtube.com/watch?v=S_MOd40zlYU',
+  },
+  {
+    title: 'Lofi Girl —  sleep radio',
+    url: 'https://www.youtube.com/watch?v=rUxyKA_-grg',
+  },
 ];
