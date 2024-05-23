37e630c2c35cccba14514cafe1acad7c81412aa4
Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
2024-05-24 01:44:17 +0200

    fix(fix space in radio): fixed the accidental space on radio names
    removal of space for radio names

--- src/data/radios.ts
+++ src/data/radios.ts
@@ -12,11 +12,11 @@ export const radios = [
     url: 'https://www.youtube.com/watch?v=4oStw0r33so',
   },
   {
-    title: 'Lofi Girl —  dream radio',
+    title: 'Lofi Girl — dream radio',
     url: 'https://www.youtube.com/watch?v=S_MOd40zlYU',
   },
   {
-    title: 'Lofi Girl —  sleep radio',
+    title: 'Lofi Girl — sleep radio',
     url: 'https://www.youtube.com/watch?v=rUxyKA_-grg',
   },
 ];
