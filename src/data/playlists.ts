0411a26c2ea4f4febec70567300f810fdfec7820
MAZE <remvze@proton.me>
2024-06-20 23:01:37 +0330

    feat: add more playlists

--- src/data/playlists.ts
+++ src/data/playlists.ts
@@ -1,6 +1,26 @@
 export const playlists = [
   {
-    title: 'Lofi Girl — dark ambient playlist',
+    title: 'Lofi Girl — Dark Ambient Playlist',
     url: 'https://soundcloud.com/lofi_girl/sets/dark-ambient-music-to-escape',
+  },
+  {
+    title: 'Lofi Girl — Peaceful Piano Playlist',
+    url: 'https://soundcloud.com/lofi_girl/sets/peaceful-piano-music-to-focus',
+  },
+  {
+    title: 'Lofi Girl — Synthwave Ambient Playlist',
+    url: 'https://soundcloud.com/lofi_girl/sets/synthwave-ambient-chill-music',
+  },
+  {
+    title: 'Lofi Girl — Lofi Reading Playlist',
+    url: 'https://soundcloud.com/lofi_girl/sets/lofi-reading-2024',
+  },
+  {
+    title: 'Chillhop Music — Night Time Cruise Playlist',
+    url: 'https://soundcloud.com/chillhopdotcom/sets/night-time-cruise',
+  },
+  {
+    title: 'Chillhop Music — Golden Hour Playlist',
+    url: 'https://soundcloud.com/chillhopdotcom/sets/golden-hour',
   },
 ];
