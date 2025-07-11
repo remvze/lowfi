14e2dff406d245367668c91ad92b2b26e941dc93
MAZE <remvze@proton.me>
2025-07-11 18:54:58 +0330

    feat: bring back the play-dl

--- src/lib/stream.ts
+++ src/lib/stream.ts
@@ -1,9 +1,15 @@
 import ora from 'ora';
 
+import {
+  stream as playStream,
+  getFreeClientID,
+  setToken,
+  SoundCloudPlaylist,
+  soundcloud,
+} from 'play-dl';
+
 import { error } from './logger';
 import { play } from './play';
-
-import Client from './scraper/client.js';
 
 import { shuffle } from '@/helpers/random';
 
@@ -13,23 +19,36 @@ export async function stream(volume: number, url: string) {
   try {
     spinner = ora('Fetching the playlist from SoundCloud').start();
 
-    const client = new Client();
-    const info = await client.getPlaylist(url);
+    const id = await getFreeClientID();
+
+    setToken({
+      soundcloud: {
+        client_id: id,
+      },
+    });
+
+    const data = await soundcloud(url);
 
     spinner.succeed('Fetched the playlist from SoundCloud');
-    console.log('');
 
-    if (info?.tracks) {
-      const { tracks } = info;
+    if (data.type === 'playlist') {
+      const spinner = ora('Fetching all the tracks from SoundCloud').start();
+
+      const pl = new SoundCloudPlaylist(data, id);
+      const tracks = await pl.all_tracks();
+
+      spinner.succeed('Fetched all the tracks from SoundCloud');
+      console.log('');
+
       const shuffled = shuffle(tracks);
 
       let index = 0;
 
       while (true) {
         const track = shuffled[index];
-        const stream = await track.downloadProgressive();
+        const { stream } = await playStream(track.url);
 
-        await play(track.title!, volume, stream);
+        await play(track.name, volume, stream);
 
         index = (index + 1) % shuffled.length;
       }
